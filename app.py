0f4cf6cac8929234699dd1ebe9d7fca5b5c8272c
Natsuki <taraponchu@gmail.com>
2016-10-07 15:49:42 +0900

    数字入力フォームを追加

--- app.py
+++ app.py
@@ -11,24 +11,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', message='Hello')
+    return render_template('index.html', message='Select the face image')
 
-@app.route('/uploader', methods=['POST', 'POST'])
+@app.route('/uploader', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
         f = request.files['the_file']
         cut_img(f)
 
 #    import pdb; pdb.set_trace()
-    path = "./static/image"
+    path = os.path.join(os.path.dirname(__file__),"static/image/")
     image_names = [relpath(img_path, path) for img_path in glob(join(path, '*.jpg'))]
     my_liist = []
     for image in image_names:
         my_dic = {}
         my_dic['image_name'] = 'static/image/' + image
         my_liist.append(my_dic)
-    return render_template('index.html', message = my_liist) 
+    return render_template('cut.html', message='Select the eye,nose and mouth', parts=my_liist) 
 
+@app.route('/uploader/select', methods=['POST'])
+def select_parts():
+    if request.method == 'POST':
+        eye = int(request.form['number1'])
+        nose = int(request.form['number2'])
+        mouth = int(request.form['number3'])
+
+    return render_template('index.html', message='Select the face image')
 
 if __name__ == "__main__":
     app.run()
